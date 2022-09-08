7c2e585b440b7d0cca9d373432b8228a3a166b96
Park Min Hyeok <opensource@pmh.codes>
2022-09-08 11:27:27 +0900

    fix: cloudfront error response

--- common-cloudfront.tf
+++ common-cloudfront.tf
@@ -76,4 +76,11 @@ resource "aws_cloudfront_distribution" "cf" {
       restriction_type = "none"
     }
   }
+
+  custom_error_response {
+    error_code = 403
+    response_code = 200
+    response_page_path = "/"
+    error_caching_min_ttl = 0
+  }
 }
