ae98902614a1052e18c1142e970e6d5e04a3e745
Park Min Hyeok <opensource@pmh.codes>
2022-09-12 23:27:37 +0900

    fix: add permission to task

--- backend-space-ecs.tf
+++ backend-space-ecs.tf
@@ -4,6 +4,7 @@ resource "aws_ecs_task_definition" "space" {
   network_mode = "awsvpc"
   cpu = 512
   memory = 1024
+  task_role_arn = aws_iam_role.space_taskrole.arn
   execution_role_arn = aws_iam_role.space_fargate.arn
 
   container_definitions = jsonencode([{
@@ -28,6 +29,46 @@ resource "aws_ecs_task_definition" "space" {
   runtime_platform {
     operating_system_family = "LINUX"
   }
+}
+
+resource "aws_iam_role" "space_taskrole" {
+  name = "parkingspace-space-task-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "space_taskrole" {
+  name = "parkingspace-space-task-policy"  
+  role = aws_iam_role.space_taskrole.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "UploadSpacefile",
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.uploads.arn}/*",
+      "Action": [
+        "s3:PutObject"
+      ]
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_iam_role" "space_fargate" {
@@ -82,14 +123,6 @@ resource "aws_iam_role_policy" "space_fargate" {
       "Action": [
         "logs:CreateLogStream",
         "logs:PutLogEvents"
-      ]
-    },
-    {
-      "Sid": "UploadSpacefile",
-      "Effect": "Allow",
-      "Resource": "${aws_s3_bucket.uploads.arn}/*",
-      "Action": [
-        "s3:PutObject"
       ]
     }
   ]
